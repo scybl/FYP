78e186da6e7fc0a7636f4c3c074bf71646a6707d
Bizare-CVIP <scybl7@163.com>
2025-02-17 10:41:27 +0800

    进行简易评估

--- params.py
+++ params.py
@@ -1,8 +1,21 @@
-from model_defination.AAA_BNet.BNet import BNet
 import torch
 
-model = BNet()
-model.load_state_dict(torch.load("bnet.pth", map_location=torch.device('cpu')))
+from model_defination.UnetBase.unetbase import UNetBase
+from model_defination.AAA_BNet.BNet import BNet
+
+
+model_name = "UNet"
+
+if model_name == "BNet":
+    model = BNet()
+    model.load_state_dict(torch.load("params/bnet_1.pth", map_location=torch.device('cpu')))
+
+
+elif model_name == "UNet": 
+    model = UNetBase()
+    model.load_state_dict(torch.load("params/unet0_1.pth", map_location=torch.device('cpu')))
+
+
 
 num_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
 print(f"Total Parameters in BNet: {num_params}")
